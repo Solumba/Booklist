42ff26781e5eba6b8282b67e73c450fe43bb0391
Olumba Solomon <Olumbasolomon08@gmail.com>
2021-01-26 08:05:37 +0100

    Project Completed

--- booklist.js
+++ booklist.js
@@ -1,3 +1,7 @@
+/* A book registry app you can add books too and delete from
+it uses local storage to ensure whatever books you add remains it
+it was always also built harnessing the perks of oop */
+
 //Book class: to create a book object when you add a book
 class Book {
     constructor(title, author, isbn){
@@ -7,12 +11,14 @@ class Book {
     }
 }
 
-/* here we create an book array and pass it to the
+/* here we create a book array and pass it to the
 local storage after searching to see if it contains any books in it
 the else statement uses the parse method when getting the books(if there are any)
 because json would return only strings so parse makes it an object or its original state
 */ 
 class DB {
+
+    //method to create local storage
     static getBooks(){
         let books;
         //checking if books are already in LS
@@ -24,39 +30,45 @@ class DB {
         return books
     }
 
-    addBooks(book){
+    //method to add books to local storage
+    static addBooks(book){
         //getting the existing books which is an empty book array
-    
         const books = DB.getBooks();
+
         //push a new book to this arr of books
         books.push(book);
-        //setting the array of books to the books key
-        //we use json. stringify because LS doesnt accept objects
+        
+        /*setting the array of books to the books key
+        we use json.stringify because LS doesnt accept objects*/
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    //method to remove books from local storage
+    static removeBooks(isbn){
+        const books = DB.getBooks();
+        books.forEach((book, index) => {
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        })
+        //we stringify our books array because ls doesn't take in anything that's not a string 
         localStorage.setItem('books', JSON.stringify(books));
     }
 }
 // create a UI class to handle UI tasks
 class UI {
+    //method to display our books from local storage
     static displayBooks(){
-        //creating a hardcoded array of books to act like local storage which would be changed later to local storage 
+        //getting our array of books from local storage
+        const books = DB.getBooks();
 
-        const StoredBooks = [
-            {
-                title: 'Book One',
-                author: 'John Doe', 
-                isbn: '3433434'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jenny Doe', 
-                isbn: '343224'
-            }
-        ];
-        const books = StoredBooks;
-        //creating a function  that would loop through all add all book objects in the list of stored and add the books to the table as tr elements 
+        /*creating a function  that would loop through all add all book objects 
+        in the list of stored and add the books to the table as tr elements */
         books.forEach((book)=> UI.addBookToList(book));
     }
+
     // the book param is already passed in the function above, that's how we are able to access its properties and rep the in td elems
+    //method to add our book props to the ui as td in tr of our table
     static addBookToList(book){
         const list = document.getElementById('book-list');
 
@@ -89,7 +101,9 @@ class UI {
         document.getElementById('isbn').value = '';
     }
 
-    //Method to create a div for alert and display it within the container , also have it delete after 3secs
+    /*Method to create a div for alert and display it within the container , also have it delete after 3secs
+    the error message is passed in dynamically so whenever the method is called, you can pass in a 
+    custom message and a custom class for your bootstrap design */
     static showAlert(message, className){
         const div = document.createElement('div');
         div.className = `alert alert-${className}`;
@@ -120,11 +134,13 @@ document.getElementById("book-form"). addEventListener('submit', (e)=>{
     const isbn = document.getElementById("isbn").value;
 
     //instantiate a book(which isnt static) and pass in the data gotten from the input fields 
-    
+    //validation to ensure user doesnt pass in empty fields
     if(title === '' || author === '' || isbn === ''){
         UI.showAlert('please add valid data', 'danger')
     }
-    else if (!Number.isInteger(isbn)) {
+
+    //validation to ensure the isbn is always a number 
+    else if (isNaN(isbn)) {
         UI.showAlert("please add a valid isbn number", 'danger');
     }
     else {
@@ -133,6 +149,8 @@ document.getElementById("book-form"). addEventListener('submit', (e)=>{
         /*add newBook  to list by calling the static method on the UI class which adds all properties of 
         new book as table data in the previosly created tr node which was appended to the table body*/
         UI.addBookToList(newBook);
+        // add book to DB
+        DB.addBooks(newBook);
         UI.clearFields();
     }
 })
@@ -141,5 +159,10 @@ document.getElementById("book-form"). addEventListener('submit', (e)=>{
 //using event propagation  to select an item being clicked within the book-list element 
 document.getElementById('book-list').addEventListener('click', (e)=>{
     UI.deleteBook(e.target);
+    
+    //textContent gets the isbn value of the element being targeted
+    DB.removeBooks(e.target.parentElement.previousElementSibling.textContent); 
     UI.showAlert("Book Deleted", "danger");
 });
+
+//Project completed
